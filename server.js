2b50e749a8d23022ef4e41abc27a2022633abf8c
fairfax <lovelace.af@gmail.com>
2021-11-01 20:11:56 -0400

    created queries to get a single candidate, delete and create entires from the db

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: '',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+//});
+
+// GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    //if (err) {
+     // console.log(err);
+   // }
+   // console.log(row);
+  //});
+// Delete a candidate from the table
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    //if (err) {
+    //    console.log(err);
+   // }
+   // console.log(result);
+//});
+
+// Create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+               // VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+
+//db.query(sql, params, (err, result) =>{
+    //if (err) {
+    //    console.log(err);
+   // }
+    //console.log(result);
+//});
 
 
 // Default response for any other request (Not Found)
